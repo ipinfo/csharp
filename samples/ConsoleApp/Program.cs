9cd5652a503686c06fa2343d59524afd1f141bf1
Soban Mahmood <soban.mahmood@mslm.io>
2022-03-07 15:48:46 +0500

    sample project template added

---
+++ samples/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        //TODO: Add sample code for using IPinfoClient
+        Console.WriteLine("Client needs to be added.");
+    }
+  }
+}
