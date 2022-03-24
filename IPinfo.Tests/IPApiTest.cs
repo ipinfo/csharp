f9b5e8696e3a3d835d68c1b588e1ba3530693218
Soban Mahmood <soban.mahmood@mslm.io>
2022-03-24 21:46:44 +0500

    added test project and test case for fetching location details against ip

---
+++ IPinfo.Tests/IPApiTest.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+using Xunit;
+
+using IPinfo;
+using IPinfo.Exceptions;
+using IPinfo.Models;
+
+namespace IPinfo.Tests;
+
+public class IPApiTest
+{
+    [Fact]
+    public void TestGetDetails()
+    {
+        string ip = "8.8.8.8";
+        IPinfoClient client = new IPinfoClient.Builder()
+        .AccessToken(Environment.GetEnvironmentVariable("IPINFO_TOKEN"))
+        .Build();
+
+        try
+        {
+            IPResponse actual = client.IPApi.GetDetails(ip);
+            
+            var expectations = new List<Tuple<object, object>>()
+            {
+                new("8.8.8.8", actual.IP),
+                new("dns.google", actual.Hostname),
+                new("Mountain View", actual.City),
+                new("California", actual.Region),
+                new("US", actual.Country),
+                new("United States", actual.CountryName),
+                new("America/Los_Angeles", actual.Timezone),
+                new("", actual.Privacy.Service),
+                new(5, actual.Domains.Domains.Count),
+            };
+            Assert.All(expectations, pair => Assert.Equal(pair.Item1, pair.Item2));
+            Assert.False(actual.Privacy.Proxy);
+            Assert.False(actual.Privacy.Vpn);
+            Assert.False(actual.Privacy.Tor);
+            Assert.False(actual.Privacy.Relay);
+            Assert.False(actual.Privacy.Hosting);            
+        }
+        catch (RequestQuotaExceededException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
