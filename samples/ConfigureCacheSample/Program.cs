7f2a889067a97d00793679b361c819e1a407b493
Soban Mahmood <soban.mahmood@mslm.io>
2022-03-30 17:16:37 +0500

    added sample project for cache configuration

---
+++ samples/ConfigureCacheSample/Program.cs
@@ -0,0 +1,53 @@
+﻿using IPinfo;
+using IPinfo.Models;
+using IPinfo.Cache;
+
+namespace ConsoleApp
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Sample for changing configuration for built-in cache");
+        
+        // To use this sample, add your IPinfo Access Token to environment variable
+        // named "IPINFO_TOKEN", or initialize your token string directly.
+        string? token = Environment.GetEnvironmentVariable("IPINFO_TOKEN");
+
+        long cacheEntryTimeToLiveInSeconds = 5;
+        int cacheSizeMbs = 2;
+        
+        if(token is not null)
+        {
+          string ip = "209.85.231.104";
+          IPinfoClient client = new IPinfoClient.Builder()
+            .AccessToken(token) // pass your token string
+            .Cache(new CacheWrapper(cacheConfig => cacheConfig
+              .CacheMaxMbs(cacheSizeMbs) // pass cache size in mbs
+              .CacheTtl(cacheEntryTimeToLiveInSeconds))) // pass time to live in seconds for cache entry
+            .Build();
+
+          int doContinue = -1;
+
+          Console.WriteLine("\nOptions:");
+          Console.WriteLine("-Enter any integer to continue");
+          Console.WriteLine("-Enter 0 to quit");
+          doContinue = Convert.ToInt32(Console.ReadLine());
+          while(doContinue != 0)
+          {
+            IPResponse ipResponse = await client.IPApi.GetDetailsAsync(ip);
+            Console.WriteLine($"IPResponse.City: {ipResponse.City}");
+            Console.WriteLine($"IPResponse.Company.Name: {ipResponse.Company.Name}");
+            Console.WriteLine($"IPResponse.Country: {ipResponse.Country}");
+            Console.WriteLine($"IPResponse.CountryName: {ipResponse.CountryName}");
+            doContinue = Convert.ToInt32(Console.ReadLine());
+          }
+        }
+        else
+        {
+          Console.WriteLine("Set your access token as IPINFO_TOKEN in environment variables in order to run this sample code. You can also set your token string in the code manually.");
+          return;
+        }
+    }
+  }
+}
