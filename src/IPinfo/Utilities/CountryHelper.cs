83deee90e25f0f399849304200b29e9c9dbec9a6
Umar Farooq <mail.umar.now@gmail.com>
2022-08-30 22:20:47 +0500

    commited code review suggestions

--- src/IPinfo/Utilities/CountryHelper.cs
+++ src/IPinfo/Utilities/CountryHelper.cs
@@ -12,7 +12,7 @@ namespace IPinfo.Utilities
     internal static class CountryHelper
     {
         private const string CountriesJsonFilePathName = "IPinfo.Utilities.Countries.json";
-        private const string EuCountriesJsonFilePathName = "IPinfo.Utilities.EuCountries.json";
+        private const string EUCountriesJsonFilePathName = "IPinfo.Utilities.EUCountries.json";
 
         /// <summary>
         /// Lazy initialization for the country dictionary object(only one instance) from country json file.
@@ -34,18 +34,18 @@ namespace IPinfo.Utilities
         /// <summary>
         /// Lazy initialization for the EUcountry List from EUcountry json file.
         /// </summary>
-        private static readonly Lazy<List<string>> eu_countries =
+        private static readonly Lazy<List<string>> s_euCountries =
         new Lazy<List<string>>(() =>
         {
-            List<string> _eu_countries;
+            List<string> euCountries;
             var assembly = Assembly.GetExecutingAssembly();
-            using (Stream stream = assembly.GetManifestResourceStream(EuCountriesJsonFilePathName))
+            using (Stream stream = assembly.GetManifestResourceStream(EUCountriesJsonFilePathName))
             using (StreamReader reader = new StreamReader(stream))
             {
                 string euCountriesJson = reader.ReadToEnd();
-                _eu_countries = JsonSerializer.Deserialize<List<string>>(euCountriesJson);
+                euCountries = JsonSerializer.Deserialize<List<string>>(euCountriesJson);
             }
-            return _eu_countries;
+            return euCountries;
         });
 
         /// <summary>
@@ -56,7 +56,7 @@ namespace IPinfo.Utilities
         /// <summary>
         /// List of EU countries.
         /// </summary>
-        private static List<string>  EuCountries { get { return eu_countries.Value; } }
+        private static List<string>  EUCountries { get { return s_euCountries.Value; } }
 
         /// <summary>
         /// Gets full country name against country code.
@@ -91,7 +91,7 @@ namespace IPinfo.Utilities
             {
                 return false;
             }
-            return EuCountries.Contains(countryCode);
+            return EUCountries.Contains(countryCode);
         }
     }
 }
